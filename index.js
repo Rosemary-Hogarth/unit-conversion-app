022c47a760fd456cd8004d582b08bd8b3d589a89
Rosemary Hogarth <rohogarth@gmail.com>
2024-10-05 10:53:49 +0200

    Refactored code

--- index.js
+++ index.js
@@ -16,22 +16,22 @@ let massEl = document.getElementById("mass-el")
 
 convertBtn.addEventListener("click", function() {
 
-  let inputNumber = inputEl.value
+
   lengthEl.innerHTML = `
     <p>
-    ${inputNumber} Meters = ${ Number(inputNumber * metersConversionRate).toFixed(3) } Feet |
-    ${inputNumber} Feet = ${ Number(inputNumber / metersConversionRate).toFixed(3) } Meters
+    ${inputEl.value} Meters = ${ Number(inputEl.value * metersConversionRate).toFixed(3) } Feet |
+    ${inputEl.value} Feet = ${ Number(inputEl.value / metersConversionRate).toFixed(3) } Meters
     </p>`
 
     volumeEl.innerHTML = `
     <p>
-    ${inputNumber} Liters = ${ Number(inputNumber * litersConversionRate).toFixed(3) } Gallons |
-    ${inputNumber} Gallons = ${Number(inputNumber / litersConversionRate).toFixed(3)} Liters
+    ${inputEl.value} Liters = ${ Number(inputEl.value * litersConversionRate).toFixed(3) } Gallons |
+    ${inputEl.value} Gallons = ${Number(inputEl.value / litersConversionRate).toFixed(3)} Liters
     </p>`
 
     massEl.innerHTML = `
     <p>
-    ${inputNumber} Kilograms = ${ Number(inputNumber * litersConversionRate).toFixed(3)} Pounds |
-    ${inputNumber} Pounds = ${ Number(inputNumber / litersConversionRate).toFixed(3)} Kilograms
+    ${inputEl.value} Kilograms = ${ Number(inputEl.value * litersConversionRate).toFixed(3)} Pounds |
+    ${inputEl.value} Pounds = ${ Number(inputEl.value / litersConversionRate).toFixed(3)} Kilograms
     </p>`
 })
